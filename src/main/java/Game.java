5b6977b1574806b4a9b7790c4caac85bc252fb5f
Salaikumar <saravanan1115@gmail.com>
2015-12-25 20:44:58 +0530

    Implemented Player Class. Game Class is incomplete. Play and endPlay methods arepending. Not sure if they need to be implemented over here

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,36 +1,27 @@
+import java.util.ArrayList;
+
 /**
  * Game Class
- * This class helps u play the actual game.
+ * Implements the methods required to play the game
  */
 public class Game {
-
-//  Players.
-    private Bot bot;
-    private Human human;
-
-//  Board
+    private Player bot;
+    private Player human;
     private Board board;
 
-
-//  Default Constructor
-//  Not sure if I need to type this.
-    public Game(){
-
-    }
-
-    public Bot getBot() {
+    public Player getBot() {
         return bot;
     }
 
-    public void setBot(Bot bot) {
+    public void setBot(Player bot) {
         this.bot = bot;
     }
 
-    public Human getHuman() {
+    public Player getHuman() {
         return human;
     }
 
-    public void setHuman(Human human) {
+    public void setHuman(Player human) {
         this.human = human;
     }
 
@@ -51,20 +42,123 @@ public class Game {
 
     }
 
-    public boolean reset(){
-        return false;
-    }
-
+    /*
+     * isOver- Checks if the Game is over.
+     * Possible Scenario: Any Player had Won the game
+     *                  : Board is full without any winners?
+     */
     public boolean isOver(){
+
+        if ( isWinnerPresent())
+            return true;
+
+        if ( board.isFull())
+            return true;
+
         return false;
     }
 
+    public boolean isWinnerPresent() {
+        return isBotWinner() || isHumanWinner();
+    }
+
+    /*
+     * Check if Bot had Win the game
+     * Possible Winning : occupy One Complete Row
+     *                  : occupy One Complete Column
+     *                  : occupy  One Complete Diagonal
+     */
     public boolean isBotWinner(){
-        return false;
+        return winByRow(bot) || winByColumn(bot) || winByDiagonal(bot);
     }
 
     public boolean isHumanWinner(){
-        return false;
+        return winByRow(human) || winByColumn(human) || winByDiagonal(human);
     }
 
+//  What is the point in me sending the properties of the same class as argument?
+//  Is that right?
+//  Let's think over it
+    private boolean winByColumn(Player player) {
+
+        Character value = player.getCellValue();
+        Boolean   colMatch = false;
+
+        ArrayList<ArrayList<Cell>> columns = board.getAllColumns();
+
+        for ( ArrayList<Cell> column : columns){
+
+            for (Cell cell : column){
+
+                 if ( value != cell.getValue()){
+                     colMatch = false;
+                     break;
+                 }
+                 colMatch = true;
+            }
+
+            if ( colMatch )
+                return true;
+        }
+
+        return  false;
+    }
+
+    private boolean winByRow(Player player) {
+        Character value = player.getCellValue();
+        Boolean   rowMatch = false;
+
+        ArrayList<ArrayList<Cell>> rows = board.getAllRows();
+
+        for ( ArrayList<Cell> row : rows){
+
+            for (Cell cell : row){
+
+                if ( value != cell.getValue()){
+                    rowMatch = false;
+                    break;
+                }
+                rowMatch = true;
+            }
+
+            if ( rowMatch )
+                return true;
+        }
+
+        return  false;
+    }
+
+    private boolean winByDiagonal(Player player) {
+        Character value = player.getCellValue();
+        Boolean   diaMatch = false;
+
+        ArrayList<ArrayList<Cell>> diagonals = board.getDiagonals();
+
+        for ( ArrayList<Cell> diagonal : diagonals){
+
+            for (Cell cell : diagonal){
+
+                if ( value != cell.getValue()){
+                    diaMatch = false;
+                    break;
+                }
+                diaMatch = true;
+            }
+
+            if ( diaMatch )
+                return true;
+        }
+
+        return  false;
+    }
+
+
+    //  This feature will be implemented later.
+//  I should just set all properties to null?
+//  Or reset the  Board alone? Clearing all values in Cells?
+//  I think the second one is more opt.
+//  Pushing to later...
+    public boolean reset(){
+        return false;
+    }
 }
---
+++ src/main/java/Player.java
@@ -0,0 +1,33 @@
+/**
+ * Player Class
+ * Since it is a two player Game, At max two instances of this class will be present
+ * I'll try to make it work and then correct the logic
+ */
+public class Player {
+
+    private String playerName;
+    private Character cellValue;
+    private boolean bot;         // bot will be true for Bot player
+
+    public Player(String playerName, Character cellValue){
+        this.playerName = playerName;
+        this.cellValue  = cellValue;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public Character getCellValue() {
+        return cellValue;
+    }
+
+    public boolean makeBot(){
+        bot = true;
+        return  true;
+    }
+
+    public boolean isBot(){
+        return bot == true;
+    }
+}
