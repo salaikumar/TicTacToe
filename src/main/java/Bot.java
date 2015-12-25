c355b43c4476a6254dcd10a19b3f4b0935e5f30e
Salaikumar <saravanan1115@gmail.com>
2015-12-25 17:15:04 +0530

    Added two players. Bot and Human

---
+++ src/main/java/Bot.java
@@ -0,0 +1,23 @@
+/**
+ * Bot is the Computer player here.
+ * He loves to play with humans. Not so intelligent by now.
+ * He learns u know :P - Stupid comments . :P
+ */
+public class Bot {
+
+    private final Character val = 'X';
+    private String name;
+
+    public Bot(String name){
+        this.name = name;
+    }
+
+    public Character getVal() {
+        return val;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
